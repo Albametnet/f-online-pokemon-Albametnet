be0cdbf5814f32f48277cb0f1d1dbf10e5dde89d
Albametnet <alhernandezsan@gmail.com>
2018-10-07 12:24:52 +0200

    all working in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.filterName = this.filterName.bind(this);
+    this.state = {
+      pokemons: [],
+      name: ''
+    }
+  }
+
+  componentDidMount() {
+    const pokeArray = [];
+    for (let i = 1; i <= 25; i++) {
+      fetch('https://pokeapi.co/api/v2/pokemon/' + i + '/')
+        .then(response => {
+          return response.json();
+        })
+
+        .then(info => {
+          pokeArray.push(info)
+          this.setState({ pokemons: [...pokeArray] });
+          console.log(this.state.pokemons);
+        })
+    }
+  }
+  filterName(e) {
+    const mypokemon = e.currentTarget.value.toLowerCase();
+    this.setState({
+      name: mypokemon
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Pokeapp de Albita
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input className="filter__input" type="text" placeholder="Search Pokemon by name"
+          onChange={this.filterName} />
+        <ul className="pokemon__list">
+          {this.state.pokemons
+            .sort((a, b) => a.id - b.id)
+            .filter(item => {
+              return item.name.toLowerCase().includes(this.state.name);
+            })
+            .map(item => {
+              return (
+                <div>
+                  <li className="pokemon__list-item">
+                    <div className="pokemon__texts">
+                      <img className='pokemon__list-pic' src={item.sprites.front_default} alt='Pokemon' />
+                      <h3 className="pokemon__list-id"> ID/{item.id}</h3>
+                      <h2 className="pokemon__list-name" >{item.name} </h2>
+                      <ul className="skills">
+                        {item.types
+                          .map(skill => {
+                            return (
+                              <li className="pokeSkill">
+                                {skill.type.name}
+                              </li>
+                            )
+                          })}
+                      </ul>
+                    </div>
+                  </li>
+                </div>
+              )
+            })}
+        </ul>
       </div>
     );
   }
---
+++ src/components/PokeList.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+
+// class CharacterList extends React.Component {
+//   render() {
+//     return (
+//       // <ul className= "character__list"> 
+//       // {this.props.results
+//       //   // .filter(item => {
+//       //   //   return item.name.toLowerCase().includes(this.props.name);
+//       //   // })
+//       //   .map(item => {
+//       //     return (
+//       //       <div>
+//       //         <li className="character__list-item">
+//       //           <div className= "character__texts">
+//       //             <h2 className="character__list-name" >{item.name} </h2>
+//       //           </div>
+//       //         </li>
+//       //       </div>
+//       //     )
+//       //   })}
+//       // </ul>
+//     );
+//   }
+// }
+
+// export default CharacterList;
