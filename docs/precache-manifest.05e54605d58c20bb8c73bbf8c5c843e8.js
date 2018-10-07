8436a3291b83017a9d3a6c97ffc7c9f67a91c244
Albametnet <alhernandezsan@gmail.com>
2018-10-07 16:29:51 +0200

    ready for github pages

---
+++ docs/precache-manifest.05e54605d58c20bb8c73bbf8c5c843e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dae359bdb9ad0b84c3b2",
+    "url": "./static/js/main.dae359bd.chunk.js"
+  },
+  {
+    "revision": "7258aa498efb1b5b3320",
+    "url": "./static/js/1.7258aa49.chunk.js"
+  },
+  {
+    "revision": "dae359bdb9ad0b84c3b2",
+    "url": "./static/css/main.98be5163.chunk.css"
+  },
+  {
+    "revision": "fd2a4f3135f709a0a9a6ebab51411c26",
+    "url": "./index.html"
+  }
+];
