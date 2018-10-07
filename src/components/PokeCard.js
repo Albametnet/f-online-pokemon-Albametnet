02ef6bd1358342c035f64872854ece7712acb80c
Albametnet <alhernandezsan@gmail.com>
2018-10-07 14:56:26 +0200

    adding some styles

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -4,25 +4,27 @@ import '../styles/PokeCard.css';
 class PokeCard extends React.Component {
   render() {
     return (
-      <div className="pokemon__card">
-        <li className="pokemon__list-item">
-          <div className="pokemon__texts">
-            <img className='pokemon__list-pic' src={this.props.item.sprites.front_default} alt='Pokemon' />
-            <h3 className="pokemon__list-id"> ID/{this.props.item.id}</h3>
-            <h2 className="pokemon__list-name" >{this.props.item.name} </h2>
-            <ul className="skills">
+      <li className="pokemon__list-item">
+        <div className="pokemon__card">
+          <div className="pokemon__item">
+            <img className='pokemon__item-pic' src={this.props.item.sprites.front_default} alt='Pokemon' />
+            <p className="pokemon__item-id"> ID/{this.props.item.id}</p>
+            </div>
+            <div className ="pokemon__info">
+            <h2 className="pokemon__info-name" >{this.props.item.name} </h2>
+            <ul className="pokemon__info-skills">
               {this.props.item.types
                 .map(skill => {
                   return (
-                    <li className="pokeSkill">
+                    <li className="pokemon__info-skill">
                       {skill.type.name}
                     </li>
                   )
                 })}
             </ul>
-          </div>
-        </li>
-      </div>
+            </div>
+        </div>
+      </li >
     );
   }
 }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -15,25 +15,6 @@ class PokeList extends React.Component {
           .map(item => {
             return (
               <PokeCard item ={item}/>
-              // <div>
-              //   <li className="pokemon__list-item">
-              //     <div className="pokemon__texts">
-              //       <img className='pokemon__list-pic' src={item.sprites.front_default} alt='Pokemon' />
-              //       <h3 className="pokemon__list-id"> ID/{item.id}</h3>
-              //       <h2 className="pokemon__list-name" >{item.name} </h2>
-              //       <ul className="skills">
-              //         {item.types
-              //           .map(skill => {
-              //             return (
-              //               <li className="pokeSkill">
-              //                 {skill.type.name}
-              //               </li>
-              //             )
-              //           })}
-              //       </ul>
-              //     </div>
-              //   </li>
-              // </div>
             )
           })}
       </ul>
